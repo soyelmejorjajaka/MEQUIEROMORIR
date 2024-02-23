358786c6da7828534f3844018ad4a59249e51215
soyelmejorjajaka <153939533+soyelmejorjajaka@users.noreply.github.com>
2024-02-23 17:32:19 +0100

    Update Mavenproject13.java

--- mavenproject13/src/main/java/com/mycompany/mavenproject13/Mavenproject13.java
+++ mavenproject13/src/main/java/com/mycompany/mavenproject13/Mavenproject13.java
@@ -11,6 +11,7 @@ package com.mycompany.mavenproject13;
 public class Mavenproject13 {
 
     public static void main(String[] args) {
-        System.out.println("Htengoganas de morird!");
+        System.out.println("Htengoganas de morirHTIIIIGRGRGRGRGRGRGRGRGRGRGRGRGRGRGRGRGRGRGRGRGR
+                           d!");
     }
 }
