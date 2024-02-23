1b5c182b35be2ebacac4c044e3e176c0b6561d62
soyelmejorjajaka <adrian.teransanchez@iesmiguelherrero.com>
2024-02-23 17:30:48 +0100

    AAAAAAAAAA

---
+++ mavenproject13/src/main/java/com/mycompany/mavenproject13/Mavenproject13.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject13;
+
+/**
+ *
+ * @author DAW109
+ */
+public class Mavenproject13 {
+
+    public static void main(String[] args) {
+        System.out.println("Htengoganas de morird!");
+    }
+}
